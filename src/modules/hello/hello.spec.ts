a72c04422007aaf71b6b7d3c011db4926bb3d852
Marcin Wysocki <wysocki.marcin92@gmail.com>
2017-06-26 23:15:20 +0200

    first test and improved coverage mapping

---
+++ src/modules/hello/hello.spec.ts
@@ -0,0 +1,41 @@
+import { HttpException } from '@nestjs/core';
+import { Test } from '@nestjs/testing';
+import { HelloController } from './hello.controller';
+import { HelloService } from './hello.service';
+
+describe('HelloModule', () => {
+    describe('HelloService', () => {
+        let service: HelloService;
+        beforeEach(() => {
+            Test.createTestingModule({
+                components: [HelloService],
+                controllers: [HelloController],
+            });
+
+            service = Test.get(HelloService);
+        });
+
+        it('greets everybody', async () => {
+            expect(await service.sayGenericHelloToNoOneInParticular()).toEqual(
+                'OH HAI',
+            );
+        });
+
+        it('greets certain person', async () => {
+            expect(await service.sayHelloToSomeone('Joe')).toEqual(
+                'Hello, Joe!',
+            );
+        });
+
+        it("doesn't talk to Marcin", async () => {
+            try {
+                await service.sayHelloToSomeone('Marcin');
+            } catch (err) {
+                expect(err).toBeInstanceOf(HttpException);
+                expect(err.response).toEqual(
+                    "We don't talk to Marcin ( ͠° ͟ʖ ͡°)",
+                );
+            }
+        });
+    });
+});
