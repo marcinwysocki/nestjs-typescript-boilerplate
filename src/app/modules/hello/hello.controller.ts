d733e66ceccf5ec77752d7a26bf69dd16f686f47
Marcin Wysocki <wysocki.marcin92@gmail.com>
2018-01-05 22:05:39 +0100

    removed @Response from the controller

--- src/app/modules/hello/hello.controller.ts
+++ src/app/modules/hello/hello.controller.ts
@@ -1,12 +1,5 @@
-import {
-    Body,
-    Controller,
-    Get,
-    HttpStatus,
-    Param,
-    Post,
-    Response,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
 import { GreetingDto } from '../../dto/GreetingDto';
 import { HelloService } from './hello.service';
 
@@ -15,25 +8,20 @@ export class HelloController {
     constructor(private helloService: HelloService) {}
 
     @Get('/')
-    public async sayHello(@Response() res) {
+    public async sayHello() {
         const greetings = await this.helloService.sayGenericHelloToNoOneInParticular();
 
-        res.status(HttpStatus.OK).json(greetings);
+        return greetings;
     }
     @Get('/:name')
-    public async sayHelloToSomeone(@Response() res, @Param('name') name) {
+    public async sayHelloToSomeone(@Param('name') name) {
         const greetings = await this.helloService.sayHelloToSomeone(name);
 
-        res.status(HttpStatus.OK).json(greetings);
+        return greetings;
     }
 
     @Post('/greeting')
-    public async postSomething(
-        @Response() res,
-        @Body() { body, prefix, suffix }: GreetingDto,
-    ) {
-        res
-            .status(HttpStatus.CREATED)
-            .json({ greeting: `${prefix},\n${body}\n${suffix}` });
+    public async postSomething(@Body() { body, prefix, suffix }: GreetingDto) {
+        return { greeting: `${prefix},\n${body}\n${suffix}` };
     }
 }
